732da386d2e70cc47ae693db155eadd54388c7c3
brian.mulier <bmmulier@hotmail.fr>
2024-06-13 12:30:13 +0200

    fix(tests): use TestsUtils.receive API instead of queue.receive

--- src/test/java/io/kestra/plugin/couchbase/TriggerTest.java
+++ src/test/java/io/kestra/plugin/couchbase/TriggerTest.java
@@ -6,6 +6,7 @@ import io.kestra.core.queues.QueueInterface;
 import io.kestra.core.repositories.LocalFlowRepositoryLoader;
 import io.kestra.core.runners.Worker;
 import io.kestra.core.schedulers.AbstractScheduler;
+import io.kestra.core.utils.TestsUtils;
 import io.kestra.jdbc.runner.JdbcScheduler;
 import io.kestra.core.services.FlowListenersInterface;
 import io.micronaut.context.ApplicationContext;
@@ -13,11 +14,11 @@ import io.kestra.core.junit.annotations.KestraTest;
 import jakarta.inject.Inject;
 import jakarta.inject.Named;
 import org.junit.jupiter.api.Test;
+import reactor.core.publisher.Flux;
 
 import java.util.Map;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.TimeUnit;
-import java.util.concurrent.atomic.AtomicReference;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
@@ -58,12 +59,8 @@ class TriggerTest extends CouchbaseTest {
                 this.flowListenersService
             );
         ) {
-            AtomicReference<Execution> last = new AtomicReference<>();
-
             // wait for execution
-            executionQueue.receive(execution -> {
-                last.set(execution.getLeft());
-
+            Flux<Execution> receive = TestsUtils.receive(executionQueue, execution -> {
                 queueCount.countDown();
                 assertThat(execution.getLeft().getFlowId(), is("couchbase-listen"));
             });
@@ -73,9 +70,10 @@ class TriggerTest extends CouchbaseTest {
 
             localFlowRepositoryLoader.load(this.getClass().getClassLoader().getResource("flows/couchbase-listen.yml"));
 
-            queueCount.await(1, TimeUnit.MINUTES);
+            boolean await = queueCount.await(1, TimeUnit.MINUTES);
+            assertThat(await, is(true));
 
-            return last.get();
+            return receive.blockLast();
         }
     }
 }
