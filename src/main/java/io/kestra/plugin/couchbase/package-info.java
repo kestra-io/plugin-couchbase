f8b27e0769ad902ba698a75d9f97ff73d862fd9a
François Delbrayelle <fdelbrayelle@gmail.com>
2026-02-13 11:39:33 +0100

    chore: update plugin categories (#79)
    * chore: update plugin categories
    
    * chore: fix plugin categories
    
    * chore: update kestraVersion to 1.2.3
    
    ---------
    
    Co-authored-by: ben8t <pimpaudben@gmail.com>
    Co-authored-by: François Delbrayelle <fdelbrayelle@kestra.io>

--- src/main/java/io/kestra/plugin/couchbase/package-info.java
+++ src/main/java/io/kestra/plugin/couchbase/package-info.java
@@ -1,7 +1,9 @@
 @PluginSubGroup(
     description = "This sub-group of plugins contains tasks for using Couchbase.\n" +
         "Couchbase is an award-winning distributed NoSQL cloud database that delivers unmatched versatility, performance, scalability, and financial value for all of your cloud, mobile, on-premises, hybrid, distributed cloud, and edge computing applications.",
-    categories = PluginSubGroup.PluginCategory.DATABASE
+    categories = {
+        PluginSubGroup.PluginCategory.DATA
+    }
 )
 package io.kestra.plugin.couchbase;
 
